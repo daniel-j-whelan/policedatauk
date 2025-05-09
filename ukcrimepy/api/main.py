8e8aa87063a492d98c8ae7bc255e17fd4704dd89
Daniel Whelan <gorillagal@protonmail.com>
2025-05-09 16:05:39 +0100

    enable caching of last updated date for front end UI purposes

--- ukcrimepy/api/main.py
+++ ukcrimepy/api/main.py
@@ -1,10 +1,10 @@
 import asyncio
 import httpx
 from aiolimiter import AsyncLimiter
+from functools import cached_property
 from crimes import CrimeAPI
 from forces import ForceAPI
 from postcodes import PostcodeAPI
-
 
 class PoliceAPI:
     def __init__(self):
@@ -17,12 +17,21 @@ class PoliceAPI:
         self.postcodes = PostcodeAPI(self.client, self.limiter, self.postcode_url)
 
 
+    @cached_property
+    def last_updated(self) -> str:
+        url = f"{self.police_url}/crime-last-updated"
+        response = self.throttle_get_request(url)
+        return response.json()["date"]
+
 async def main():
     api = PoliceAPI()
     # Example usage
     crime_categories = await api.crimes.get_crime_categories()
     print(crime_categories)
 
+    force_summaries = await api.forces.get_all_forces()
+    for force in force_summaries:
+        print(force.name)
 
 if __name__ == "__main__":
     asyncio.run(main())
