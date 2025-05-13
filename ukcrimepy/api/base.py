ddf684848af03fb1b4ba6ee372c35bdca0fdf1c7
Daniel Whelan <gorillagal@protonmail.com>
2025-05-13 18:47:24 +0100

    improve docstrings

--- ukcrimepy/api/base.py
+++ ukcrimepy/api/base.py
@@ -1,18 +1,28 @@
-import httpx
+from httpx import AsyncClient, HTTPStatusError, Response
 from aiolimiter import AsyncLimiter
 
 class BaseAPI:
-    def __init__(self, client: httpx.AsyncClient, limiter: AsyncLimiter, base_url: str):
+    def __init__(self, client: AsyncClient, limiter: AsyncLimiter, base_url: str):
         self.client = client
         self.limiter = limiter
         self.base_url = base_url
         
-    async def _throttle_get_request(self, url: str) -> httpx.Response:
-        """Throttle the numebr of GET requests to avoid overwhelming the endpoint."""
+    async def _throttle_get_request(self, url: str) -> Response:
+        """Perform a GET request with rate limiting.
+
+        Args:
+            url (str): URL of the request.
+
+        Returns:
+            Response: The server response.
+
+        Exceptions:
+            HTTPStatusError: If the response status code is 429 (rate limit exceeded).
+        """
         async with self.limiter:
             response = await self.client.get(url)
             if response.status_code == 429:
-                raise httpx.HTTPStatusError(
+                raise HTTPStatusError(
                     "Rate limit exceeded (429)",
                     request=response.request,
                     response=response,
