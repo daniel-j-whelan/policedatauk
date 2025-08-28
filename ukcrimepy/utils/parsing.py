7b0afdf70e2a79ebc1dec8b74fe201caf6355dde
Daniel Whelan <gorillagal@protonmail.com>
2025-08-28 09:55:42 +0100

    parse polygons to match format required by API

--- ukcrimepy/utils/parsing.py
+++ ukcrimepy/utils/parsing.py
@@ -1,4 +1,6 @@
 import re
+from shapely.geometry import Polygon
+from shapely import wkt
 from typing import Tuple
 
 LAT_LON_REGEX = re.compile(
@@ -37,3 +39,30 @@ def parse_lat_lon(coord: str) -> Tuple[float, float]:
         raise ValueError(f"Could not parse lat/lon from: {coord!r}")
     lat, lon = map(float, match.groups())
     return lat, lon
+
+
+def parse_polygon(polygon: str | Polygon) -> str:
+    """Parse a polygon string into the required format.
+
+    Args:
+        polygon: The polygon string to parse.
+
+    Returns:
+        str: The formatted polygon string ready for police data UK API use.
+
+    Raises:
+        ValueError if the polygon string is not valid.
+    """
+    if isinstance(polygon, Polygon):
+        if not polygon.is_valid:
+            raise ValueError("Invalid polygon provided.")
+        api_polygon = ":".join(f"{lat},{lon}" for lon, lat in polygon.exterior.coords[:-1])
+    elif isinstance(polygon, str):
+        polygon_obj = wkt.loads(polygon)
+        if not polygon_obj.is_valid:
+            raise ValueError("Invalid polygon provided.")
+        api_polygon = ":".join(
+            f"{lat},{lon}" for lon, lat in polygon_obj.exterior.coords[:-1]
+        )
+
+    return api_polygon
