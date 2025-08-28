b4bf74286e86455dbb4f7daebb4881bd14e96df6
Daniel Whelan <gorillagal@protonmail.com>
2025-08-28 15:21:49 +0100

    include both get and post methods due to API limitations

--- ukcrimepy/api/base.py
+++ ukcrimepy/api/base.py
@@ -27,8 +27,35 @@ class BaseAPI:
         Exceptions:
             HTTPStatusError: If the response status code is 429 (rate limit exceeded).
         """
+        headers = {"Content-Type": "application/json"}
         async with self.limiter:
-            response = await self.client.post(url, data=data)
+            response = await self.client.post(url, json=data, headers=headers)
+            if response.status_code == 429:
+                raise HTTPStatusError(
+                    "Rate limit exceeded (429)",
+                    request=response.request,
+                    response=response,
+                )
+            response.raise_for_status()
+            return response
+
+    @async_retry()
+    async def _throttle_get_request(
+        self, url: str, params: dict | None = None
+    ) -> Response:
+        """Perform a GET request with rate limiting.
+
+        Args:
+            url: URL of the request.
+
+            params: The query parameters for the GET request.
+                Defaults to None.
+
+        Returns:
+            The server response.
+        """
+        async with self.limiter:
+            response = await self.client.get(url, params=params)
             if response.status_code == 429:
                 raise HTTPStatusError(
                     "Rate limit exceeded (429)",
