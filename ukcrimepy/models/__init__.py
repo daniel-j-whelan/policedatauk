2cf0039b325ce1291f96967345ca30f8a82b135c
Daniel Whelan <gorillagal@protonmail.com>
2025-08-29 11:22:20 +0100

    add commonly used models to init

--- ukcrimepy/models/__init__.py
+++ ukcrimepy/models/__init__.py
@@ -1,4 +1,13 @@
-from .crime import CrimeCategory, CrimeReport
+from .crime import CrimeCategory, CrimeReport, CrimeWithOutcomes
 from .force import Force, ForceSummary, Person
+from .postcode import PostCode
 
-__all__ = ["CrimeCategory", "CrimeReport", "Force", "ForceSummary", "Person"]
+__all__ = [
+    "CrimeCategory",
+    "CrimeReport",
+    "CrimeWithOutcomes",
+    "Force",
+    "ForceSummary",
+    "Person",
+    "PostCode",
+]
