4ac1720ae22420979209a2a197c28fdec34d2aaf
Daniel Whelan <gorillagal@protonmail.com>
2025-09-02 13:31:44 +0100

    add BaseAPI to init to enable users to create their own classes

--- ukcrimepy/api/__init__.py
+++ ukcrimepy/api/__init__.py
@@ -1,6 +1,7 @@
+from .base import BaseAPI
 from .crimes import CrimeAPI
 from .forces import ForceAPI
 from .police import PoliceAPI
 from .postcodes import PostcodeAPI
 
-__all__ = ["CrimeAPI", "ForceAPI", "PostcodeAPI", "PoliceAPI"]
+__all__ = ["BaseAPI", "CrimeAPI", "ForceAPI", "PostcodeAPI", "PoliceAPI"]
