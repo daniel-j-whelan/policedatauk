ee03bce8f9af4a9908727f1474532e891269ca25
Daniel Whelan <gorillagal@protonmail.com>
2025-09-03 15:10:48 +0100

    delete old ukcrimepy instances

--- ukcrimepy/api/__init__.py
+++
@@ -1,7 +0,0 @@
-from .base import BaseAPI
-from .crimes import CrimeAPI
-from .forces import ForceAPI
-from .police import PoliceAPI
-from .postcodes import PostcodeAPI
-
-__all__ = ["BaseAPI", "CrimeAPI", "ForceAPI", "PostcodeAPI", "PoliceAPI"]
