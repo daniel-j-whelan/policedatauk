27b0aa40c77e0bfed7f9afdf496fc30cd94fd18f
Daniel Whelan <gorillagal@protonmail.com>
2025-09-04 15:21:15 +0100

    add pydantic_to_df

--- policedatauk/utils/__init__.py
+++ policedatauk/utils/__init__.py
@@ -1,9 +1,11 @@
+from .dataframe import pydantic_to_df
 from .geo import buffer_point
 from .retry import async_retry
 from .parsing import parse_lat_lon, parse_polygon
 from .validation import validate_date, validate_lat, validate_lon
 
 __all__ = [
+    "pydantic_to_df",
     "validate_date",
     "validate_lat",
     "validate_lon",
