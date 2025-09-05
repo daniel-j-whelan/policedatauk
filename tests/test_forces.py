11652dd78f2f8d0e991b469b6e4e619ae2dfc8ce
Daniel Whelan <gorillagal@protonmail.com>
2025-09-05 12:54:19 +0100

    ruff check and format

---
+++ tests/test_forces.py
@@ -0,0 +1,35 @@
+"""Tests for forces-related functionality."""
+
+import pytest
+from respx import MockRouter
+
+from policedatauk import PoliceClient
+
+
+@pytest.mark.asyncio
+async def test_get_all_forces(
+    api_client: PoliceClient, police_mock_respx: MockRouter
+) -> None:
+    """Tests that the get_all_forces method returns the expected result.
+
+    Args:
+        api_client (PoliceClient): The PoliceClient instance.
+        mock_respx (Mock): The respx mock.
+    """
+    mock_route = police_mock_respx.get("/forces").respond(
+        200,
+        json=[
+            {
+                "id": "avon-and-somerset",
+                "name": "Avon and Somerset Constabulary",
+            },
+            {"id": "bedfordshire", "name": "Bedfordshire Police"},
+        ],
+    )
+
+    forces = await api_client.forces.get_all_forces()
+
+    assert len(forces) == 2
+    assert forces[0].id == "avon-and-somerset"
+    assert forces[1].name == "Bedfordshire Police"
+    assert mock_route.called
