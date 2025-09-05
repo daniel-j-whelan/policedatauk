d6b68d09487eb8e902a87347a384d30ab98b6880
Daniel Whelan <gorillagal@protonmail.com>
2025-09-05 10:20:37 +0100

    test crimes with a location provided

--- policedatauk/tests/test_crimes.py
+++ policedatauk/tests/test_crimes.py
@@ -1,8 +1,11 @@
+import polars as pl
 import pytest
+
+from policedatauk.utils.dataframe import pydantic_to_df
 
 
 @pytest.mark.asyncio
-async def test_get_crimes_no_location(api_client, mock_respx):
+async def test_crimes_no_location(api_client, mock_respx):
     mock_route = mock_respx.post("/crimes-no-location").respond(
         200,
         json=[
@@ -48,3 +51,64 @@ async def test_get_crimes_no_location(api_client, mock_respx):
         == "1b793a5cb34177d9dc9cc693ec026d04d8fe0910f31f7c062f3ea17c7f3057c5"
     )
     assert mock_route.called
+
+
+async def test_crimes_by_location(api_client, mock_respx):
+    mock_route = mock_respx.post("/crimes-street/all-crime").respond(
+        200,
+        json=[
+            {
+                "category": "anti-social-behaviour",
+                "location_type": "Force",
+                "location": {
+                    "latitude": "52.343315",
+                    "street": {
+                        "id": 2043533,
+                        "name": "On or near Kennedy Road",
+                    },
+                    "longitude": "0.417594",
+                },
+                "context": "",
+                "outcome_status": None,
+                "persistent_id": "",
+                "id": 115918022,
+                "location_subtype": "",
+                "month": "2024-01",
+            },
+            {
+                "category": "anti-social-behaviour",
+                "location_type": "Force",
+                "location": {
+                    "latitude": "52.342091",
+                    "street": {
+                        "id": 2043502,
+                        "name": "On or near Supermarket",
+                    },
+                    "longitude": "0.409276",
+                },
+                "context": "",
+                "outcome_status": None,
+                "persistent_id": "",
+                "id": 115918552,
+                "location_subtype": "",
+                "month": "2024-01",
+            },
+        ],
+    )
+
+    crimes = await api_client.crimes.get_crimes_by_location(
+        poly="52.268,0.543:52.794,0.238:52.130,0.478",
+        date="2024-01",
+    )
+    assert len(crimes) == 2
+    assert crimes[0].id == 115918022
+    assert crimes[1].category == "anti-social-behaviour"
+
+    crimes_df = pydantic_to_df(crimes, "crime_reports")
+    assert isinstance(crimes_df, pl.DataFrame)
+    assert mock_route.called
+
+
+async def test_no_geo_params(api_client):
+    with pytest.raises(ValueError):
+        await api_client.crimes.get_crimes_by_location()
