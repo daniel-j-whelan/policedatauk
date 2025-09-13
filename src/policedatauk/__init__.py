def44d7b9f773c81d5c9c8e546a7edb611684e1a
Daniel Whelan <gorillagal@protonmail.com>
2025-09-13 11:56:50 +0100

    include both sync and async options to init

--- src/policedatauk/__init__.py
+++ src/policedatauk/__init__.py
@@ -1,15 +1,22 @@
 """Initialisation file for the policedatauk package."""
 
-from policedatauk.api.crimes import CrimeAPI
-from policedatauk.api.forces import ForceAPI
-from policedatauk.api.police import PoliceClient
-from policedatauk.api.postcodes import PostcodeAPI
-from policedatauk.utils import async_retry
+from policedatauk.api.client import AsyncClient, Client
+from policedatauk.api.crimes import AsyncCrimeAPI, CrimeAPI
+from policedatauk.api.forces import AsyncForceAPI, ForceAPI
+from policedatauk.api.neighbourhoods import AsyncNeighbourhoodAPI, NeighbourhoodAPI
+from policedatauk.api.postcodes import AsyncPostcodeAPI, PostcodeAPI
+from policedatauk.utils import retry_with_backoff
 
 __all__ = [
+    "AsyncClient",
+    "Client",
+    "AsyncCrimeAPI",
     "CrimeAPI",
+    "AsyncForceAPI",
     "ForceAPI",
+    "AsyncNeighbourhoodAPI",
+    "NeighbourhoodAPI",
+    "AsyncPostcodeAPI",
     "PostcodeAPI",
-    "PoliceClient",
-    "async_retry",
+    "retry_with_backoff",
 ]
--- src/policedatauk/api/__init__.py
+++ src/policedatauk/api/__init__.py
@@ -1,17 +1,25 @@
 """Initialisation file for the api submodule."""
 
 from .base import BaseAPI
-from .crimes import CrimeAPI
-from .forces import ForceAPI
-from .neighbourhoods import NeighbourhoodAPI
-from .police import PoliceClient
-from .postcodes import PostcodeAPI
+from .client import AsyncClient, Client
+from .crimes import AsyncCrimeAPI, CrimeAPI
+from .forces import AsyncForceAPI, ForceAPI
+from .neighbourhoods import AsyncNeighbourhoodAPI, NeighbourhoodAPI
+from .postcodes import AsyncPostcodeAPI, PostcodeAPI
+from .transports import AsyncTransport, Transport
 
 __all__ = [
+    "AsyncClient",
+    "AsyncTransport",
+    "Client",
     "BaseAPI",
+    "AsyncCrimeAPI",
     "CrimeAPI",
+    "AsyncForceAPI",
     "ForceAPI",
+    "AsyncNeighbourhoodAPI",
     "NeighbourhoodAPI",
-    "PoliceClient",
+    "AsyncPostcodeAPI",
     "PostcodeAPI",
+    "Transport",
 ]
