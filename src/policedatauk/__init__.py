34d00b9043017807fdc86fb43a48da1408509a1f
Daniel Whelan <gorillagal@protonmail.com>
2026-03-29 22:16:23 +0100

    update client naming conventions

--- src/policedatauk/__init__.py
+++ src/policedatauk/__init__.py
@@ -1,25 +1,8 @@
 """Initialisation file for the policedatauk package."""
 
-from policedatauk.api.client import AsyncClient, Client
-from policedatauk.api.resources.base import BaseResource
-from policedatauk.api.resources.crimes import AsyncCrimes, Crimes
-from policedatauk.api.resources.forces import AsyncForces, Forces
-from policedatauk.api.resources.neighbourhoods import (
-    AsyncNeighbourhoods,
-    Neighbourhoods,
-)
-from policedatauk.api.resources.postcodes import AsyncPostcodes, Postcodes
+from policedatauk.api.client import AsyncPoliceClient, PoliceClient
 
 __all__ = [
-    "AsyncClient",
-    "Client",
-    "BaseResource",
-    "AsyncCrimes",
-    "Crimes",
-    "AsyncForces",
-    "Forces",
-    "AsyncNeighbourhoods",
-    "Neighbourhoods",
-    "AsyncPostcodes",
-    "Postcodes",
+    "AsyncPoliceClient",
+    "PoliceClient",
 ]
