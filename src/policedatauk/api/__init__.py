41cbf2c8ca1c1f9e91ecc3d98514bdad7df79ae3
Daniel Whelan <gorillagal@protonmail.com>
2026-03-29 22:17:30 +0100

    update client naming conventions

--- src/policedatauk/api/__init__.py
+++ src/policedatauk/api/__init__.py
@@ -1,6 +1,6 @@
 """Initialisation file for the api submodule."""
 
-from .client import AsyncClient, Client
+from .client import AsyncPoliceClient, PoliceClient
 from .resources.base import BaseResource
 from .resources.crimes import AsyncCrimes, Crimes
 from .resources.forces import AsyncForces, Forces
@@ -8,6 +8,8 @@ from .resources.neighbourhoods import AsyncNeighbourhoods, Neighbourhoods
 from .resources.postcodes import AsyncPostcodes, Postcodes
 
 __all__ = [
+    "AsyncPoliceClient",
+    "PoliceClient",
     "BaseResource",
     "AsyncCrimes",
     "Crimes",
